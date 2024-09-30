4e4c90f3badc0e04d02d1f9eede1b84294443049
Ragav Srinivasan <56784771+Rageon787@users.noreply.github.com>
2024-09-30 11:13:59 +1000

    Solution for Counting Rooms

--- graph-algos/counting-rooms.cpp
+++ graph-algos/counting-rooms.cpp
@@ -1,38 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std; 
 
-void solve(int i, int j, vector<string> &building, map<pair<int,int>,int>  visited) {
-	if (visited[{i, j}] || building[i][j] == '#') return; 
-	visited[{i, j}] = 1;  
-	if (i + 1 < building.size()) solve(i + 1, j, building, visited);   
-	if (i - 1 < building.size()) solve(i + 1, j, building, visited);   
-	if (j + 1 < building[0].size()) solve(i, j + 1, building, visited);   
-	if (j - 1 < building[0].size()) solve(i, j - 1, building, visited);   
- 
-} 
+void dfs(int i, int j, vector<vector<bool>> &visited, vector<string> &grid) { 
+	if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size()) return; 
 
-int main() { 
+	if (visited[i][j] || grid[i][j] == '#') return; 
+	visited[i][j] = true;
 
+	vector<pair<int,int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}}; // north, south, east, west
+
+	for (auto dir: directions) {
+		dfs(i + dir.first, j + dir.second, visited, grid);	
+	}
+}
+
+int main() {
 	int n, m; 
 	cin >> n >> m; 
-	vector<string> building;	 
-	unordered_map<pair<int,int>, int> visited; 
-
-	for (int i = 0; i < n; i++) {
-		string s; 
-		cin >> s;  
-		building.push_back(s);
-	} 
 	
-	int ans = 0; 
-
+	vector<string> grid(n); 
+	
+	for (int i = 0; i < n; i++) cin >> grid[i];
+	
+	int ans = 0;
+	vector<vector<bool>> visited(n, vector<bool>(m, false));
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			if (visited[{i, j}] == 0) {
-				solve(i, j, building, visited);
-			}
+			if (!visited[i][j] && grid[i][j] == '.') {
+				dfs(i, j, visited, grid);
+				ans++; 
+			} 
 		}
 	} 
 
 	cout << ans << endl;
+
 }
